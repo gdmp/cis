7f4c38972fcca5e446f7d9103f9141ccea439b3b
xhuang <otomadmt@icloud.com>
2019-08-10 05:07:09 +0900

    + Initial data_util.py

---
+++ data_util.py
@@ -0,0 +1,32 @@
+"""
+Filename: data_util.py
+Maintainers: otomadmt
+
+A data util for Kaggle competition 2019 Summer.
+
+"""
+
+import numpy as np
+
+
+class DataLoader:
+    pass
+
+
+class DataSampler:
+
+    def sample(self):
+        """
+        Input an iterator and partitions to sample.
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        """
+        pass
+
+    def reservoir(self):
+        pass
